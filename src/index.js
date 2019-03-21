b1724b71369a0ac0ca423dc3b343b7f39c1c60bd
Vlad Tataritsky <tataritskii@bk.ru>
2019-03-21 19:22:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 module.exports = function getZerosCount(number, base) {
+    return findZerosOfNumberByBase(number, getPrimesWithPow(base));
+}
 
-    var fact = 1;
-    var count = 0;
-    var arr = [];
-
-    for (var i = 1; i <= number; i++) {
-        fact = fact * i;
-    }
-
-
-    while (fact !== 0) {
-        var newSS = fact % base;
-        fact = Math.trunc(fact / base);
-        var newArr = arr.push(newSS);
-    }
-
-
-    for (var j = 0; j < arr.length; j++) {
-        if (arr[j] == 0) {
-            count++
+var findZerosOfNumberByBase = function(number, primesWithPow) {
+    let trailingZeros;
+    for (var [prime, pow] of primesWithPow) {
+        let numbersOfMultiples = 0;
+        for (let numberOfMultiples = 1, i = 1; i <= number && numberOfMultiples >= 1; i++) {
+            numberOfMultiples = Math.floor(number / Math.pow(prime, i));
+            numbersOfMultiples += numberOfMultiples;
         }
-        else {
-            break;
-        }
+        let sumOfNumbersOfMultiples = Math.floor(numbersOfMultiples / pow);
+        trailingZeros = trailingZeros < sumOfNumbersOfMultiples ? trailingZeros : sumOfNumbersOfMultiples;
     }
+    return trailingZeros;
+}
 
-    return count;
+var getPrimesWithPow = function(base) {
+    const primesWithPow = new Map();
+    for (let pow = 0, number = 2; number <= base; number++, pow = 0) {
+        for (;isPrime(number) && base % number === 0; pow++, base /= number);
+        primesWithPow.set(number, pow);
+    }
+    return primesWithPow;
+}
 
-
+var isPrime = function(number) {
+    for(var i = 2; i < number; i++)
+        if(number % i === 0) return false;
+    return number > 1;
 }
